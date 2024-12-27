4aa4174aaebd1a2adcd1df27ea9b0832f71ec1aa
cmgvc <chloe.gavv@gmail.com>
2024-12-27 14:23:05 -0500

    add link to cms

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,20 @@
+import { LinkedinIcon } from 'lucide-react'
 import React from 'react'
 
 function Footer() {
     return (
         <div className='footer bg-[#112a4f] h-[22vh] z-[1] flex justify-between p-[5%]'> 
-        <a href='#home' className='text-white hover:text-gray-300'>Back to top</a>
-        <div className='flex'>
-            <a href='#about' className='text-white hover:text-gray-300'>About</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <a href='#experience' className='text-white hover:text-gray-300'>Experience</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <a href='#projects' className='text-white hover:text-gray-300'>Projects</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <a href='#Contact' className='text-white hover:text-gray-300'>Contact</a>
-        </div>
-            <p className='text-white'>© 2024 Gaurav Dogra</p>
+            <a href='#home' className='text-white hover:text-gray-300'>Back to top</a>
+            <div className='flex'>
+                <a href='#about' className='text-white hover:text-gray-300'>About</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a href='#experience' className='text-white hover:text-gray-300'>Experience</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a href='#projects' className='text-white hover:text-gray-300'>Projects</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <a href='#Contact' className='text-white hover:text-gray-300'>Contact</a>
+            </div>
+            <div className='flex flex-col text-right z-[1]'>
+                <p className='text-white'>© 2024 Gaurav Dogra</p>
+                <a href='https://gaurav-cms.vercel.app/' className='text-white hover:text-gray-300 underline'>Sign in to edit</a>
+            </div>
         </div>
     )
 }
